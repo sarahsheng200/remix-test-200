c35853a6725475fb236f2b14382b85df56d7a495
sarahsheng200 <sarahsheng723@gmail.com>
2025-07-31 16:49:55 +0800

    add stake logic

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -5,16 +5,123 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MyToken is ERC20, Ownable {
+
+    struct Stake {
+        uint256 amount;          // 质押数量
+        uint256 startTime;       // 质押开始时间
+        uint256 endTime;         // 质押结束时间
+        uint256 rewardRate;      // 收益率（根据期限计算）
+        bool isActive;           // 订单是否有效
+    }
+
+    struct timeMapping {
+        uint256 periodDays;
+        uint256 time;
+    }
+
+    enum StakingPeriod { 
+        ThirtyDays,
+        NinetyDays, 
+        HundredEightyDays, 
+        OneYear
+    }
+
+    mapping(StakingPeriod => uint256) public apy; // 年化收益率（百分比，如20表示20%）
+    mapping(address => mapping(uint256=>Stake)) public userStakes; // 用户的所有质押订单
+
+    event Staked(
+        address indexed user,
+        uint256 amount,
+        StakingPeriod period,
+        uint256 timestamp,
+        uint256 stakeIndex
+    );
+
+    event Withdrawn(
+        address indexed user,
+        uint256 totalAmount,
+        uint256 stakeIndex
+    );
+
     constructor(
         string memory name,
         string memory symbol,
         uint256 initialSupply
     ) ERC20(name, symbol) Ownable(msg.sender)  { // 关键修改：将name和symbol传递给ERC20构造函数
         _mint(msg.sender, initialSupply * 10 ** decimals());
+        apy[StakingPeriod.ThirtyDays] = 10;   // 10% 年化
+        apy[StakingPeriod.NinetyDays] = 15;   // 15% 年化
+        apy[StakingPeriod.HundredEightyDays] = 20;      // 20% 年化
+        apy[StakingPeriod.OneYear] = 25;      // 20% 年化
     }
 
     // 可选：添加增发功能（仅所有者可调用）
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
+    }
+
+    function stake(uint256 amount, StakingPeriod period) external {
+        require(amount>=0, "Insufficient balance");
+        require(super.balanceOf(msg.sender)>=amount, "Insufficient balance");
+
+        timeMapping memory durMapping = _getDuration(period);
+        uint256 dur=durMapping.time;
+        uint256 periodDays=durMapping.periodDays;
+        uint256 startT=block.timestamp;
+        uint256 endT= startT + dur;
+        uint256 index=dur+startT;      
+
+        emit Staked(msg.sender, amount, period, endT,index);
+
+        userStakes[msg.sender][index]=Stake({
+            amount:amount,
+            startTime:startT,
+            endTime:endT,         // 质押结束时间
+            rewardRate:apy[period]*periodDays/360,     // 收益率（根据期限计算）
+            isActive:true           // 订单是否有效
+        });
+    }
+
+    // 内部函数：根据期限返回秒数
+    function _getDuration(StakingPeriod period) internal pure returns (timeMapping memory) {
+
+        if(period==StakingPeriod.ThirtyDays){
+            return timeMapping({
+                periodDays:30,
+                time:5 minutes
+            });
+        }else if(period==StakingPeriod.NinetyDays){
+            return timeMapping({
+                periodDays:90,
+                time:10 minutes
+            });
+        }else if(period==StakingPeriod.HundredEightyDays){
+            return timeMapping({
+                periodDays:180,
+                time:15 minutes
+            });
+        }else if(period==StakingPeriod.OneYear){
+            return timeMapping({
+                periodDays:360,
+                time:20 minutes
+            });
+        }else{
+            return timeMapping({
+                periodDays:360,
+                time:1 hours
+            });
+        }
+
+    }
+
+    function withdraw(uint256 stakeIndex) external {
+        Stake memory s = userStakes[msg.sender][stakeIndex];
+        require(s.isActive, "Stake is not active or already withdrawn");
+        require(block.timestamp >= s.endTime, "Stake period is not ended");
+        uint256 totalAmount=s.amount*(1+s.rewardRate/100);
+
+        userStakes[msg.sender][stakeIndex].isActive=false;
+
+        emit Withdrawn(msg.sender,totalAmount,stakeIndex);
     }
 }
