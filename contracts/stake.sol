b763cf0446c675e77b3639cecb3bd4f06674f922
sarahsheng200 <sarahsheng723@gmail.com>
2025-08-08 14:08:31 +0800

    change file name

--- contracts/stake.sol
+++ contracts/stake.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyToken is ERC20, Ownable {
+contract MtkContracts  {
 
     struct Stake {
         uint256 amount;          // 质押数量
@@ -12,6 +12,7 @@ contract MyToken is ERC20, Ownable {
         uint256 endTime;         // 质押结束时间
         uint256 rewardRate;      // 收益率（根据期限计算）
         bool isActive;           // 订单是否有效
+        uint256 stakeIndex;
     }
 
     struct timeMapping {
@@ -25,10 +26,15 @@ contract MyToken is ERC20, Ownable {
         HundredEightyDays, 
         OneYear
     }
+    
+    IERC20 public stakingToken; 
 
     mapping(StakingPeriod => uint256) public apy; // 年化收益率（百分比，如20表示20%）
     mapping(address => mapping(uint256=>Stake)) public userStakes; // 用户的所有质押订单
-    mapping(address => uint256) public nextStakeId; // 自增计数器
+    mapping(uint256=>address) public stakeOwnerMapping;
+    mapping(address => uint256[]) public userStakeIndexes;
+
+    uint256 private nonce;// 自增计数器
 
     event Staked(
         address indexed user,
@@ -45,33 +51,32 @@ contract MyToken is ERC20, Ownable {
     );
 
     constructor(
-        string memory name,
-        string memory symbol,
-        uint256 initialSupply
-    ) ERC20(name, symbol) Ownable(msg.sender)  { 
-        _mint(msg.sender, initialSupply * 10 ** decimals());
+        IERC20 _mtkToken
+    )  { 
+        stakingToken=_mtkToken;
+
         apy[StakingPeriod.ThirtyDays] = 10;   // 10% 年化
         apy[StakingPeriod.NinetyDays] = 15;   // 15% 年化
         apy[StakingPeriod.HundredEightyDays] = 20;      // 20% 年化
         apy[StakingPeriod.OneYear] = 25;      // 20% 年化
     }
 
-    // 可选：添加增发功能（仅所有者可调用）
-    function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount);
-    }
+
 
     function stake(uint256 amount, StakingPeriod period) external {
         uint256 amountWei=amount*10**18;
         require(amount>0, "Amount should be positive");
-        require(balanceOf(msg.sender)>=amountWei, "Insufficient balance");
+        require(stakingToken.balanceOf(msg.sender)>=amountWei, "Insufficient balance");
+        require(stakingToken.transferFrom(msg.sender, address(this), amount),"Staking: Transfer failed");
+       
 
         timeMapping memory durMapping = _getDuration(period);
         uint256 dur=durMapping.time;
         uint256 periodDays=durMapping.periodDays;
         uint256 startT=block.timestamp;
         uint256 endT= startT + dur;
-        uint256 index =nextStakeId[msg.sender]++;
+        uint256 index =_generateStakeId();
+        uint256 rate=apy[period]*periodDays*10**18/360;
 
         emit Staked(msg.sender, amount, period, endT,index);
 
@@ -79,9 +84,13 @@ contract MyToken is ERC20, Ownable {
             amount:amount,
             startTime:startT,
             endTime:endT,         // 质押结束时间
-            rewardRate:apy[period]*periodDays*10**18/360,     // 收益率（根据期限计算）
-            isActive:true           // 订单是否有效
+            rewardRate:rate,     // 收益率（根据期限计算）
+            isActive:true,           // 订单是否有效
+            stakeIndex:index
         });
+        stakeOwnerMapping[index]=msg.sender;
+        userStakeIndexes[msg.sender].push(index);
+        
     }
 
     // 内部函数：根据期限返回秒数
@@ -117,13 +126,49 @@ contract MyToken is ERC20, Ownable {
     }
 
     function withdraw(uint256 stakeIndex) external {
+        require(stakeOwnerMapping[stakeIndex]==msg.sender, "You are not the owner of this stake");
+
         Stake storage s = userStakes[msg.sender][stakeIndex];
+       
         require(s.isActive, "Stake is not active or already withdrawn");
         require(block.timestamp >= s.endTime, "Stake period is not ended");
+        
         uint256 reward=s.amount*s.rewardRate/100/10** 18;
         uint256 totalAmount=s.amount+reward;
         s.isActive=false;
-
         emit Withdrawn(msg.sender,totalAmount,stakeIndex);
     }
+
+    // 生成唯一的质押ID
+    function _generateStakeId() internal returns (uint256) {
+        return uint256(keccak256(abi.encodePacked(block.timestamp, msg.sender, nonce++)));
+    }
+    
+    // 获取用户所有活跃的质押
+    function getUserActiveStakes(address user) external view returns (Stake[] memory) {       
+        
+        uint256 activeCount;
+
+        for(uint256 i=0;i<userStakeIndexes[user].length;i++){
+            uint256 index=userStakeIndexes[user][i];
+            if(userStakes[user][index].isActive){
+                activeCount++;
+            }
+
+        }
+        Stake[] memory activeStakes = new Stake[](activeCount);
+        uint256 activeIndex=0;
+
+        for (uint256 i = 0; i < userStakeIndexes[user].length; i++) {
+            uint256 index=userStakeIndexes[user][i];
+            Stake storage s = userStakes[user][index];
+            if (s.isActive) {
+                activeStakes[activeIndex]=s;
+                activeIndex++;
+            }
+        }
+        
+        return activeStakes;
+    }
+    
 }
